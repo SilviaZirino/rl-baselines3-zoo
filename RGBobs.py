cb644eb8415bba295f431fbb934b59c48860826c
Silvia Zirino <silvia.zirino@mail.polimi.it>
2021-03-29 16:31:08 +0200

    custom wrapper

--- RGBobs.py
+++ RGBobs.py
@@ -18,7 +18,7 @@ class RGBobs(Wrapper):
         N_CHANNELS = 1
         HEIGHT = 64
         WIDTH = 64
-        obs_shape = (N_CHANNELS, HEIGHT, WIDTH)
+        obs_shape = (HEIGHT, WIDTH, N_CHANNELS)
         self.observation_space = spaces.Box(low = 0, high = 255, shape = obs_shape, dtype=np.uint8)
 
 
@@ -27,12 +27,12 @@ class RGBobs(Wrapper):
         obs = self.env.render('rgb')
         obs_gray = rgb2gray(obs)
         obs = transform.resize(obs_gray, (64, 64))
-        return obs[None, :, :]*255
+        return obs[:, :, None]*255
 
     def step(self,action):
         obs, reward, done, info = self.env.step(action)
         obs = self.env.render('rgb')
         obs_gray = rgb2gray(obs)
         obs = transform.resize(obs_gray, (64, 64))
-        return obs[None, :, :]*255, reward, done, info
+        return obs[:, :, None]*255, reward, done, info
 
